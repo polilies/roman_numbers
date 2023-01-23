7436041263223f8c2d214e0432d19ebb76437e93
Alican KAYIKCI <alijan84@hotmail.com>
2023-01-23 19:26:29 +0300

    some improvements

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, redirect, url_for, render_template, request
 
 
-def int_to_roman():
+def int_to_roman(num):
     # Storing roman values of digits from 0-9
     # when placed at different places
     m = ["", "M", "MM", "MMM"]
@@ -29,24 +29,25 @@ app = Flask(__name__)
 
 
 #
-@app.route('/result', methods=['GET'])
+@app.route('/roman', methods=['GET'])
 def roman(number_decimal, number_roman):
-    return render_template('result.html', number_decimal, number_roman, developer_name="Alican KAYIKCI")
+    return render_template('result.html', number_decimal=number_decimal, number_roman=number_roman, developer_name="Alican KAYIKCI")
  
 
 
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        number = int(request.form['number'])
-        
-        if 0< number <3999:
+        number = int(request.form['number'].title())
+        print(f"number is : {number}")
+        if 0 < number < 3999:
+            
             roman_number = int_to_roman(number)
             return redirect(url_for('roman', number_decimal=number, number_roman= roman_number))
         else:
             return render_template('index.html', not_valid = True)
     else:
-        return render_template('index.html', not_valid = False)
+        return render_template('index.html', not_valid = False, developer_name="Alican KAYIKCI")
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
